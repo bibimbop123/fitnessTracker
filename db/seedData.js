b66ea451355abe2fa8adf4e3cbf3e8320454231f
briankim <brianhkim13@gmail.com>
2023-05-23 11:42:49 -0500

    SeedData

--- db/seedData.js
+++ db/seedData.js
@@ -1,8 +1,17 @@
 const users = [
   { username: "Daniel", password: 123456 },
   { username: "Brian", password: 12345678 },
+  { username: "pawan", password: 12345678 },
+  { username: "collin", password: 12345678 },
+  { username: "Chris", password: 12345678 },
 ];
-const activities = [];
+const activities = [
+  { id: "1", name: "squats", description: "LEGS" },
+  { id: "2", name: "lunges", description: "LEGS" },
+  { id: "3", name: "deadlifts", description: "LEGS" },
+  { id: "4", name: "calf raises", description: "LEGS" },
+  { id: "5", name: "hip thrusts/bridges", description: "LEGS" },
+];
 const routines = [
   {
     creator_id: 1,
@@ -10,6 +19,7 @@ const routines = [
     name: "lift weights",
     goal: "get swole",
   },
+  {},
 ];
 const routine_activities = [];
 
