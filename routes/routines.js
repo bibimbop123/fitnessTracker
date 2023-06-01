eb95532a3a96aa3baeea0c0b5866633ee86f6ba3
briankim <brianhkim13@gmail.com>
2023-06-01 11:07:09 -0500

    routinesRouter.post

--- routes/routines.js
+++ routes/routines.js
@@ -1,5 +1,5 @@
 const routinesRouter = require("express").Router();
-const { getAllRoutines } = require("../db/adapters/routines");
+const { getAllRoutines, createRoutine } = require("../db/adapters/routines");
 
 routinesRouter.get("/", async (req, res, next) => {
   try {
@@ -9,4 +9,13 @@ routinesRouter.get("/", async (req, res, next) => {
     next(error);
   }
 });
+routinesRouter.post("/", async (req, res, next) => {
+  try {
+    const createdRoutine = await createRoutine({ creator_id, is_public, goal });
+    res.send(createdRoutine);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = routinesRouter;
