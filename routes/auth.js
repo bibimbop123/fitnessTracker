f409b9c134fcccaa1c94e4364300d76a0df6d994
briankim <brianhkim13@gmail.com>
2023-06-03 16:13:20 -0500

    changed auth routes to users routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const authRouter = require("express").Router();
 const bcrypt = require("bcrypt");
 const SALT_ROUNDS = 10;
-const { createUser, getUserByUsername } = require("../db/adapters/users.js");
+
 const { authRequired } = require("./utils.js");
 
 const jwt = require("jsonwebtoken");
@@ -9,65 +9,6 @@ const { JWT_SECRET } = process.env;
 require("dotenv").config();
 
 //this is where we create token
-
-// POST /api/auth/register
-authRouter.post("/register", async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-
-    const _user = await getUserByUsername(username);
-    if (_user) {
-      next({
-        message: "That user already exists!",
-        name: " Auth Error",
-      });
-      return;
-    }
-
-    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-    console.log("hashed password:", hashedPassword);
-    const user = await createUser({ username, password: hashedPassword });
-
-    console.log("JWT secret:", process.env.JWT_SECRET);
-
-    const token = jwt.sign(user, process.env.JWT_SECRET);
-    console.log("token:", token);
-
-    res.cookie("token", token, {
-      sameSite: "strict",
-      httpOnly: true,
-      signed: true,
-    });
-    delete user.password;
-    res.send({ user, token });
-  } catch (error) {
-    next(error);
-  }
-});
-
-authRouter.post("/login", async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-
-    const user = await getUserByUsername(username);
-    const checkedpassword = await bcrypt.compare(password, user.password);
-    if (checkedpassword) {
-      delete user.password;
-      const token = jwt.sign(user, process.env.JWT_SECRET);
-      res.cookie("token", token, {
-        sameSite: "strict",
-        httpOnly: true,
-        signed: true,
-      });
-      res.send(user);
-    } else {
-      next({ message: "invalid login credentials" });
-      return;
-    }
-  } catch (error) {
-    next(error);
-  }
-});
 
 authRouter.get("/logout", async (req, res, next) => {
   try {
@@ -83,10 +24,6 @@ authRouter.get("/logout", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-});
-
-authRouter.get("/me", authRequired, async (req, res, next) => {
-  res.send(req.user);
 });
 
 // app.get("/authenticate", (req, res, next) => {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,18 @@
+const { authRequired } = require("./utils.js");
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 10;
+
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 const usersRouter = require("express").Router();
 const { getPublicRoutinesByUser } = require("../db/adapters/routines");
-const { getAllUsers } = require("../db/adapters/users");
+const {
+  getAllUsers,
+  createUser,
+  getUserByUsername,
+} = require("../db/adapters/users");
 
 usersRouter.get("/", async (req, res, next) => {
   try {
@@ -19,6 +31,66 @@ usersRouter.get("/:username/routines", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+});
+usersRouter.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const _user = await getUserByUsername(username);
+    if (_user) {
+      next({
+        message: "That user already exists!",
+        name: " Auth Error",
+      });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+    console.log("hashed password:", hashedPassword);
+    const user = await createUser({ username, password: hashedPassword });
+
+    console.log("JWT secret:", process.env.JWT_SECRET);
+
+    const token = jwt.sign(user, process.env.JWT_SECRET);
+    console.log("token:", token);
+
+    res.cookie("token", token, {
+      sameSite: "strict",
+      httpOnly: true,
+      signed: true,
+    });
+    delete user.password;
+    res.send({ user, token });
+  } catch (error) {
+    next(error);
+  }
+});
+usersRouter.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await getUserByUsername(username);
+    const checkedpassword = await bcrypt.compare(password, user.password);
+    if (checkedpassword) {
+      delete user.password;
+      const token = jwt.sign(user, process.env.JWT_SECRET);
+      res.cookie("token", token, {
+        sameSite: "strict",
+        httpOnly: true,
+        signed: true,
+      });
+      res.send(user);
+    } else {
+      next({ message: "invalid login credentials" });
+      return;
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get("/me", authRequired, async (req, res, next) => {
+  res.send(req.user);
 });
 
 module.exports = usersRouter;
