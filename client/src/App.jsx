f95da0b6b6074ab14b8bc8f5e1283aef779a601c
Daniel H <dannyhdz27@gmail.com>
2023-06-09 00:22:21 -0500

    merged branches

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,12 @@ import Home from "./components/Home";
 import RoutinesComponent from "./components/Routines";
 import Nav from "./components/Nav";
 import AuthForm from "./components/auth/AuthForm";
-import ActivitiesComponent from "./components/Activities";
+// import ActivitiesComponent from "./components/Activities";
 
 function App() {
   return (
     <div>
-      <nav>
-        <Link to="/">HOME</Link>
-        <Link to="/routines">Routines</Link>
-      </nav>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<AuthForm />} />
